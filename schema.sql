0a2283233a1b3e40c5bb9a12c6ce04c8869bff37
otaxhu <oscarperniamoreno@gmail.com>
2023-08-14 23:35:55 -0400

    feat: the schema for the mysql database

---
+++ schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS go_htmx_project;
+
+CREATE DATABASE go_htmx_project;
+
+USE go_htmx_project;
+
+CREATE TABLE products(
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(1024) NOT NULL,
+    image_url VARCHAR(255) NOT NULL
+);
