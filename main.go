b1bcdb6f306878fe3713b6b0bc8858ed7310aee8
otaxhu <oscarperniamoreno@gmail.com>
2023-08-18 15:43:27 -0400

    fix: injecting all the dependencies correctly

--- main.go
+++ main.go
@@ -1,52 +1,34 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 
-	"github.com/otaxhu/go-htmx-project/internal/models"
 	"github.com/otaxhu/go-htmx-project/internal/repository"
+	"github.com/otaxhu/go-htmx-project/internal/service"
+	"github.com/otaxhu/go-htmx-project/internal/web"
 	"github.com/otaxhu/go-htmx-project/settings"
 )
 
 func main() {
-	dbSettings, err := settings.NewDatabase()
-	if err != nil {
-		log.Fatalln(err)
-	}
 	serverSettings, err := settings.NewServer()
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
-	fmt.Println(dbSettings, serverSettings)
+	dbSettings, err := settings.NewDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
 	productsRepo, err := repository.NewProductsRepository(dbSettings)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
-	products := []models.Product{
-		{
-			Name:        "producto 1",
-			Description: "description 1",
-			ImageUrl:    "url 1",
-		},
-		{
-			Name:        "producto 2",
-			Description: "description 2",
-			ImageUrl:    "url 2",
-		},
-		{
-			Name:        "producto 3",
-			Description: "description 3",
-			ImageUrl:    "url 3",
-		},
-		{
-			Name:        "producto 4",
-			Description: "description 4",
-			ImageUrl:    "url 4",
-		},
+	imageRepo := repository.NewImageRepository()
+	productsService := service.NewProductsService(productsRepo, imageRepo)
+	app, err := web.NewWebApp(serverSettings, productsService)
+	if err != nil {
+		log.Fatal(err)
 	}
-	for _, product := range products {
-		productsRepo.UpsertProduct(context.TODO(), product)
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
 	}
 }
