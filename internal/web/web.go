403c0f7848aa18375007f1d6c2674926001d565c
otaxhu <oscarperniamoreno@gmail.com>
2023-09-16 23:01:29 -0400

    feat(WebApp interface): new methods SetViews() and SetTemplateFuncs() for injecting inside of the interface

--- internal/web/web.go
+++ internal/web/web.go
@@ -3,6 +3,7 @@ package web
 import (
 	"embed"
 	"fmt"
+	"html/template"
 
 	"github.com/otaxhu/go-htmx-project/internal/service"
 	"github.com/otaxhu/go-htmx-project/internal/web/implementations"
@@ -10,16 +11,15 @@ import (
 )
 
 type WebApp interface {
+	SetViews(viewsFS embed.FS)
+	SetTemplateFuncs(funcs template.FuncMap) error
 	Start() error
 }
-
-//go:embed views/*
-var views embed.FS
 
 func NewWebApp(serverSettings settings.Server, productsService service.ProductsService) (WebApp, error) {
 	switch serverSettings.Framework {
 	case "fiber":
-		return implementations.NewFiberApp(serverSettings, productsService, views), nil
+		return implementations.NewFiberApp(serverSettings, productsService), nil
 	default:
 		return nil, fmt.Errorf("the `%s` framework does not have a `WebApp` implementation", serverSettings.Framework)
 	}
